af6d2eb1df1ab19b99d16dd10c4efa55e72b6061
luusi <luciana.silo@gmail.com>
2023-05-20 19:34:06 +0200

    adding services, target definition

---
+++ mdp/types.py
@@ -0,0 +1,17 @@
+"""
+This module contains the main type definitions.
+
+In particular:
+- State is the type of state; we require the
+"""
+from typing import Any, Dict, Hashable, Tuple
+
+State = Hashable
+Action = Any
+Reward = float
+Prob = float
+TransitionFunction = Dict[State, Dict[Action, State]]
+MDPDynamics = Dict[State, Dict[Action, Tuple[Dict[State, Prob], Reward]]]
+TargetDynamics = Dict[State, Dict[Action, Tuple[State, Prob, Reward]]]
+
+#transition_function: Dict[StateType, Dict[SymbolType, StateType]],
